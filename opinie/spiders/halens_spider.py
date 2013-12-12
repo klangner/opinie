e0a864cbd64b77a714186d0b4b489d6ade800d5c
Krzysztof Langner <klangner@gmail.com>
2013-12-12 15:31:15 +0100

    Dodane notatki oraz szkielet nowego spidera. Próbne uruchomienie na VPS

---
+++ opinie/spiders/halens_spider.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 30-11-2013
+
+@author: Krzysztof Langner
+'''
+from opinie.items import ReviewItem
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders.crawl import Rule, CrawlSpider
+from scrapy.http.request import Request
+from scrapy.spider import BaseSpider
+from scrapy import log
+import json
+import re
+
+
+class HalensSpider(CrawlSpider):
+    name = "halens"
+    allowed_domains = ["halens.pl"]
+    start_urls = ["http://www.halens.pl"]
+    rules = (
+        Rule(SgmlLinkExtractor(allow=('/produkt/')), callback='parse_product'),
+        Rule(SgmlLinkExtractor(allow=())),
+    )
+    product_url = 'http://www.halens.pl/moda-damska-sukienki-5818/sukienka-055382'
+    counter = 1
+    
+    def parse_product(self, response):
+#        log.msg(response.url, level=log.INFO)
+        m = re.search('(?<=productId:)\d+', response.body)
+        if m:
+            productId = m.group(0)
+            return Request(url=self.product_url + "/ajax/sortByDate?productId=" + productId,
+                       callback=self.parse_json)
+        return []
+    
+    def parse_json(self, response):
+        reviews = json.loads(response.body_as_unicode())
+        log.msg("%d. Found %d reviews" % (self.counter, len(reviews)), level=log.INFO)
+        self.counter += 1
+        items = []
+        for review in reviews:
+            item = ReviewItem()
+            item['id'] = "%s/%s#%s" % (self.product_url, review['productId'], review['id'])
+            item['text'] = review['opinion']
+            item['score'] = review['rating']
+            item['useful'] = review['useful']
+            item['notUseful'] = review['notUseful']
+            items.append(item)
+        return items    
+    
+    
+class HalensProductSpider(BaseSpider):
+    name = "halenstest"
+    allowed_domains = ["halens.pl"]
+    start_urls = ["http://www.halens.pl/moda-damska-sukienki-5818/sukienka-055382"]
+    
+    def parse(self, response):
+        self.dump(response.body)
+        items = []
+#        for review in reviews:
+#            item = ReviewItem()
+#            item['id'] = "%s/%s#%s" % (self.product_url, review['productId'], review['id'])
+#            item['text'] = review['opinion']
+#            item['score'] = review['rating']
+#            item['useful'] = review['useful']
+#            item['notUseful'] = review['notUseful']
+#            items.append(item)
+        return items    
+    
+    def dump(self, text):
+        f = open('output/halens.html', 'w')
+        f.write(text)
+        f.close()
