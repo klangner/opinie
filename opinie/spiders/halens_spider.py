09228b0922e9b006b327939f05ff7f1a07280903
Krzysztof Langner <klangner@gmail.com>
2013-12-15 10:50:44 +0100

    some cleaning

--- opinie/spiders/halens_spider.py
+++ opinie/spiders/halens_spider.py
@@ -4,14 +4,9 @@ Created on 30-11-2013
 
 @author: Krzysztof Langner
 '''
-from opinie.items import ReviewItem
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders.crawl import Rule, CrawlSpider
-from scrapy.http.request import Request
 from scrapy.spider import BaseSpider
-from scrapy import log
-import json
-import re
 
 
 class HalensSpider(CrawlSpider):
@@ -26,27 +21,7 @@ class HalensSpider(CrawlSpider):
     counter = 1
     
     def parse_product(self, response):
-#        log.msg(response.url, level=log.INFO)
-        m = re.search('(?<=productId:)\d+', response.body)
-        if m:
-            productId = m.group(0)
-            return Request(url=self.product_url + "/ajax/sortByDate?productId=" + productId,
-                       callback=self.parse_json)
-        return []
-    
-    def parse_json(self, response):
-        reviews = json.loads(response.body_as_unicode())
-        log.msg("%d. Found %d reviews" % (self.counter, len(reviews)), level=log.INFO)
-        self.counter += 1
         items = []
-        for review in reviews:
-            item = ReviewItem()
-            item['id'] = "%s/%s#%s" % (self.product_url, review['productId'], review['id'])
-            item['text'] = review['opinion']
-            item['score'] = review['rating']
-            item['useful'] = review['useful']
-            item['notUseful'] = review['notUseful']
-            items.append(item)
         return items    
     
     
@@ -57,16 +32,7 @@ class HalensProductSpider(BaseSpider):
     
     def parse(self, response):
         self.dump(response.body)
-        items = []
-#        for review in reviews:
-#            item = ReviewItem()
-#            item['id'] = "%s/%s#%s" % (self.product_url, review['productId'], review['id'])
-#            item['text'] = review['opinion']
-#            item['score'] = review['rating']
-#            item['useful'] = review['useful']
-#            item['notUseful'] = review['notUseful']
-#            items.append(item)
-        return items    
+        return []    
     
     def dump(self, text):
         f = open('output/halens.html', 'w')
